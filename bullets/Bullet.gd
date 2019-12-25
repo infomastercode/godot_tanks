082312af20a58f7414320a95d733add0d4b3d87e
infomastercode <infomastercode@gmail.com>
2019-12-25 18:29:09 +0700

    update

--- bullets/Bullet.gd
+++ bullets/Bullet.gd
@@ -23,6 +23,5 @@ func _on_Bullet_body_entered(body):
 	if body.has_method('take_damage'):
 		body.take_damage(damage)
 
-
 func _on_Lifetime_timeout():
 	explode()
--- maps/Map.gd
+++ maps/Map.gd
@@ -15,3 +15,4 @@ func _on_Tank_shoot(bullet, _position, _direction):
 	var b = bullet.instance()
 	add_child(b)
 	b.start(_position, _direction)
+
